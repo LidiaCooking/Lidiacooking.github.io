c4adbca89a281ffce6ef880fd7eaaca54589bab5
LidiaCooking <139638255+LidiaCooking@users.noreply.github.com>
2023-08-30 23:58:14 +0300

    Delete Car repair  up directory

--- Car repair  up/js/script.js
+++
@@ -1,5 +0,0 @@
-let menuBtn = document.querySelector('.burger__btn');
-let menu = document.querySelector('.burger__menu-wrapper');
-menuBtn.addEventListener('click', function(){
-	menu.classList.toggle('active');
-})
